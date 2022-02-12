066c37de7efae959f859c692c5a71e998ffd59fb
Paulin <paulinlendi2000@gmail.com>
2022-02-12 07:17:05 +0100

    initialistation2

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,8 @@ import Navigation from '../components/Navigation';
 const About = () => {
   return (
     <div>
-      <h1>A propos</h1>
       <Navigation/>
+      <h1>A propos</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti accusamus saepe iure sit eum, rerum deserunt minima asperiores eos ipsa hic ducimus eius similique consequuntur sint dolorem dolore. Fugit, a.</p>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Countries from '../components/Countries';
 import Navigation from '../components/Navigation';
 
 const Home = () => {
   return (
     <div>
-      <h1>Accueil</h1>
       <Navigation/>
+      <h1>Accueil</h1>
+      <Countries/>
+      
     </div>
   )
 };
